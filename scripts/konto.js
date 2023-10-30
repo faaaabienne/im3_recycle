a09b5bffa873e4932332954362523a1681d8ffda
faaaabienne <146833916+faaaabienne@users.noreply.github.com>
2023-10-30 20:25:50 +0100

    blup

--- scripts/konto.js
+++ scripts/konto.js
@@ -1,11 +1,44 @@
-// Annahme: user ist der angemeldete Benutzer (mithilfe von Supabase auth)
-const User = supabase.auth.user();
+import { supa } from "../../../scripts/konto.js";
 
-if (User) {
+async function showMovies() {s
+  const ul = document.querySelector('#list');
+  const { data: movies, error } = await supa.from("movies").select();
+  movies.forEach(movie => {
+    const li = document.createElement('li');
+    li.innerHTML = movie.title;
+    ul.appendChild(li);
+  })
+}
+showMovies();
+
+async function showActors() {
+  const table = document.querySelector('#table');
+  const { data: actors, error } = await supa.from("actors").select();
+  actors.forEach(actor => {
+    const output = `
+      <tr>
+        <td>${user.first_name}</td>
+        <td>${user.last_name}</td>
+        <td>${user.email}</td>
+        <td>${user.user_points}</td>
+      </tr>
+    `;
+    table.innerHTML += output;
+  })
+}
+showActors();
+
+
+
+
+// Annahme: user ist der angemeldete Benutzer (mithilfe von Supabase auth)
+const user = supabase.auth.user();
+
+if (user) {
   // Der Benutzer ist angemeldet, hole seine Daten aus der Tabelle 'users'
   const { data, error } = await supabase
-    .from('users')
-    .select('name, email') // Wähle die Spalten, die du benötigst
+    .from('User')
+    .select('first_name, last_name, email, user_points') // Wähle die Spalten, die du benötigst
     .eq('id', user.id);    // Filtere nach der ID des angemeldeten Benutzers
 
   if (error) {
@@ -13,9 +46,12 @@ if (User) {
   } else {
     // Annahme: data enthält die Benutzerdaten als Objekt
     const userData = data[0]; // Wir nehmen an, es gibt nur einen Benutzer mit dieser ID
-    // Jetzt kannst du die Daten in deine HTML-Elemente einfügen
-    document.getElementById('name').textContent = `Name: ${userData.name}`;
+
+    // Setze die Daten in die HTML-Elemente auf der konto.html Seite
+    document.getElementById('first_name').textContent = `Vorname: ${userData.name}`;
+    ocument.getElementById('last_name').textContent = `Name: ${userData.name}`;
     document.getElementById('email').textContent = `Email: ${userData.email}`;
+    document.getElementById('user_points').textContent = `Punktestand: ${userData.points}`;
   }
 } else {
   // Der Benutzer ist nicht angemeldet
