4cebb73a72d5d9a89214e8c7690a0ab8b4cca031
faaaabienne <146833916+faaaabienne@users.noreply.github.com>
2023-10-30 19:47:42 +0100

    bö

--- scripts/konto.js
+++ scripts/konto.js
@@ -1,5 +1,23 @@
-function meineFunktion() {
-    alert('Hallo, das ist eine Benachrichtigung!');
-}
+// Annahme: user ist der angemeldete Benutzer (mithilfe von Supabase auth)
+const User = supabase.auth.user();
 
-console.log(meineFunktion)
+if (User) {
+  // Der Benutzer ist angemeldet, hole seine Daten aus der Tabelle 'users'
+  const { data, error } = await supabase
+    .from('users')
+    .select('name, email') // Wähle die Spalten, die du benötigst
+    .eq('id', user.id);    // Filtere nach der ID des angemeldeten Benutzers
+
+  if (error) {
+    console.error(error.message);
+  } else {
+    // Annahme: data enthält die Benutzerdaten als Objekt
+    const userData = data[0]; // Wir nehmen an, es gibt nur einen Benutzer mit dieser ID
+    // Jetzt kannst du die Daten in deine HTML-Elemente einfügen
+    document.getElementById('name').textContent = `Name: ${userData.name}`;
+    document.getElementById('email').textContent = `Email: ${userData.email}`;
+  }
+} else {
+  // Der Benutzer ist nicht angemeldet
+  // Führe hier entsprechende Aktionen aus (z.B. Umleitung zur Anmeldeseite)
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,52 @@
 import { supa } from "../config/config.js";
 
+const supabaseUrl = 'https://sdbjufroocuobdtvassq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYmp1ZnJvb2N1b2JkdHZhc3NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMzY3MzIsImV4cCI6MjAxMTkxMjczMn0.iLluyCY0Q3IrvYmAr_eYOwwbGCUr5rYwbcpnvbW62AA';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+async function login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const { user, error } = await supabase.auth.signIn({ email, password });
+    if (user) {
+        showDashboard(user);
+    } else {
+        alert(error.message);
+    }
+}
+
+async function register() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const { user, error } = await supabase.auth.signUp({ email, password });
+    if (user) {
+        showDashboard(user);
+    } else {
+        alert(error.message);
+    }
+}
+
+function showDashboard(user) {
+    document.getElementById('auth').hidden = true;
+    document.getElementById('dashboard').hidden = false;
+    document.getElementById('userEmail').textContent = user.email;
+    // Get and display points (from Supabase DB)
+}
+
+async function addPoints() {
+    // Add points to user in Supabase DB and display updated points
+}
+
+async function logout() {
+    await supabase.auth.signOut();
+    document.getElementById('auth').hidden = false;
+    document.getElementById('dashboard').hidden = true;
+}
+
+// Check if user is already logged in
+const user = supabase.auth.user();
+if (user) {
+    showDashboard(user);
+}
