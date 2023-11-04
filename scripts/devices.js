7a6be40473e33ad9a6c814f6b23aaa98c65bdf63
faaaabienne <146833916+faaaabienne@users.noreply.github.com>
2023-11-04 13:19:48 +0100

    css und js

--- scripts/devices.js
+++ scripts/devices.js
@@ -1,1 +1,20 @@
 import { supa } from "../config/config.js";
+
+
+const kategorie = document.querySelector('#kategorie');
+btn.addEventListener('click', insertInput);
+
+
+async function insertInput() {
+  const input = document.querySelector ('#first_name');
+
+  const { data: actors, error } = await supa.from("category").insert([
+    {
+      'user_id': 'input.value',
+      'category_id': 'kategorie.value'
+    }
+  ]);
+  return actors;
+}
+
+
