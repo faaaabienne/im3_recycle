d8e0f1b1c61fd2437a6bf31b8cf480f749430bcc
myripopyri <146833714+myripopyri@users.noreply.github.com>
2023-11-09 22:48:58 +0100

    css_nurno_halbfrustriert

--- scripts/signup.js
+++ scripts/signup.js
@@ -24,17 +24,39 @@ async function signUp() {
 const signUpButton = document.getElementById('signUpButton');
 signUpButton.addEventListener('click', signUp);
 
-// JavaScript-Code, um den Footer anzuzeigen, wenn der Benutzer ans Ende der Seite scrollt
+// JavaScript-Code, um den Footer allmählich anzuzeigen, wenn der Benutzer ans Ende der Seite scrollt
+var footer = document.querySelector('footer');
+var footerHidden = true; // Um zu verfolgen, ob der Footer gerade angezeigt oder ausgeblendet ist
+
 window.addEventListener('scroll', function() {
-    var footer = document.querySelector('footer');
-    var windowHeight = window.innerHeight;
-    var bodyHeight = document.body.offsetHeight;
-    var scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-  
-    // Überprüfen, ob der Benutzer ganz unten auf der Seite ist
-    if (scrollPosition + windowHeight >= bodyHeight) {
-      footer.classList.remove('footer-hidden'); // Footer anzeigen
-    } else {
-      footer.classList.add('footer-hidden'); // Footer ausblenden
-    }
-  });
+  var windowHeight = window.innerHeight;
+  var bodyHeight = document.body.offsetHeight;
+  var scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+  var scrollTrigger = bodyHeight - windowHeight; // Trigger-Punkt, wenn der Footer allmählich angezeigt/ausgeblendet werden soll
+
+  // Überprüfen, ob der Benutzer nahe genug am Ende der Seite ist
+  if (scrollPosition >= scrollTrigger && footerHidden) {
+    // Den Footer allmählich einblenden
+    footerHidden = false;
+    footer.style.opacity = '0'; // Startwert der Opazität auf 0 setzen
+    footer.classList.remove('footer-hidden');
+    requestAnimationFrame(function() {
+      footer.style.transition = 'opacity 0.5s'; // CSS-Übergang hinzufügen
+      footer.style.opacity = '1'; // Die Opazität auf 1 erhöhen, um den Footer anzuzeigen
+    });
+  } else if (scrollPosition < scrollTrigger && !footerHidden) {
+    // Den Footer allmählich ausblenden
+    footerHidden = true;
+    footer.style.opacity = '1'; // Startwert der Opazität auf 1 setzen
+    requestAnimationFrame(function() {
+      footer.style.transition = 'opacity 0.5s'; // CSS-Übergang hinzufügen
+      footer.style.opacity = '0'; // Die Opazität auf 0 verringern, um den Footer auszublenden
+      footer.addEventListener('transitionend', function() {
+        if (footerHidden) {
+          footer.classList.add('footer-hidden'); // Den Footer ausblenden, nachdem der Übergang abgeschlossen ist
+          footer.style.transition = ''; // Übergang entfernen, um für das Einblenden vorzubereiten
+        }
+      }, { once: true });
+    });
+  }
+});
